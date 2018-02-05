0e73b26a83b80ce458b33a81a0db3776e48c15a9
kgalexis <phadomduck@gmail.com>
2018-02-05 21:19:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import LaplaceLM.py
 #import KneserNeyLM.py
 
+def read_corpus_file(path, start, end):  
+    file = open(path, 'r', encoding="utf8")
+    content = list(file)[start:end]
+    file.close()
+    return content
+
+filepath = "/home/kostas/Desktop/Semester 2/text engineering analytics/assignments/assignment1/el-en/europarl-v7.el-en.en"
+train_set = read_corpus_file(filepath, 0, 5000)
+test_set = read_corpus_file(filepath, 5000, 7000)
+
 for n in range(2,4):
     lm = LaplaceLM(train_set, n)
     lm.test(test_set)
