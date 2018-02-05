54be68cca28207d3284370aad1bccaa90a46dda6
alexZeakis <alzeakis@gmail.com>
2018-02-05 23:14:39 +0200

    Interpolation & KNLM Language models called.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import LaplaceLM.py
-#import KneserNeyLM.py
+from LaplaceLM import LaplaceLM
+from InterpolationLM import InterpolationLM
+from KNLM import KNLM
+import numpy as np
 
 def read_corpus_file(path, start, end):  
     file = open(path, 'r', encoding="utf8")
@@ -11,6 +13,14 @@ filepath = "/home/kostas/Desktop/Semester 2/text engineering analytics/assignmen
 train_set = read_corpus_file(filepath, 0, 5000)
 test_set = read_corpus_file(filepath, 5000, 7000)
 
+for l in np.arange(0,1.1,.2):
+    print("Trying lambda: {}".format(l))
+    lm = InterpolationLM(train_set, lambda1=l)
+    lm.eval_measures(test_set)
+
+#lm = KNLM(train_set,n)
+#lm.test(test_set)
+'''
 for n in range(2,4):
     lm = LaplaceLM(train_set, n)
     lm.test(test_set)
@@ -18,3 +28,4 @@ for n in range(2,4):
     for predict_sequence in predict_sequences:
         lm.predict(predict_sequence)
     lm.eval_measures(test_set)
+'''
