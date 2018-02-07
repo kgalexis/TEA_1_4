0a569b803877f59b0b09af3bcb774fd3b24abeac
kgalexis <phadomduck@gmail.com>
2018-02-08 00:25:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 from LaplaceLM import LaplaceLM
-from InterpolationLM import InterpolationLM
-from KNLM import KNLM
+from InterpolatedLM import InterpolatedLM
 import numpy as np
 
-def read_corpus_file(path, start, end):  
-    file = open(path, 'r', encoding="utf8")
-    content = list(file)[start:end]
-    file.close()
-    return content
+def read_file(path, start, end):
+    with open(path, 'r') as file:
+        return list(file)[start:end]
 
-filepath = "/home/kostas/Desktop/Semester 2/text engineering analytics/assignments/assignment1/el-en/europarl-v7.el-en.en"
-train_set = read_corpus_file(filepath, 0, 5000)
-test_set = read_corpus_file(filepath, 5000, 7000)
+filepath = '/home/kostas/Desktop/Semester 2/text engineering analytics/assignments/assignment1/el-en/europarl-v7.el-en.en'
+train_set = read_file(filepath, 0, 3000)
+test_set = read_file(filepath, 3000, 4000)
 
-for l in np.arange(0,1.1,.2):
-    print("Trying lambda: {}".format(l))
-    lm = InterpolationLM(train_set, lambda1=l)
-    lm.eval_measures(test_set)
+print('\n========== Bigram Language Model ==========')
+bigram_lm = LaplaceLM(train_set, 2, 5)
 
-#lm = KNLM(train_set,n)
-#lm.test(test_set)
-'''
-for n in range(2,4):
-    lm = LaplaceLM(train_set, n)
-    lm.test(test_set)
-    predict_sequences = ["Show a great", "I would report", "I would like"]
-    for predict_sequence in predict_sequences:
-        lm.predict(predict_sequence)
-    lm.eval_measures(test_set)
-'''
+correct_sentence = bigram_lm.get_random_sentence(test_set)
+print("{}\n---Log-probability: {}\n".format(correct_sentence, bigram_lm.test(correct_sentence)))
+test_sequences = bigram_lm.generate_test_sequences(len(correct_sentence.split()))
+for test_sequence in test_sequences:
+    print("{}\n\n---Log-probability: {}\n".format(test_sequence, bigram_lm.test(test_sequence)))
+
+predict_sequences = ["Show a great", "This will take", "I would like"]
+for predict_sequence in predict_sequences:
+    print(predict_sequence)
+    bigram_lm.predict(predict_sequence)
+
+bigram_lm.evaluate(test_set)
+
+print('\n========== Trigram Language Model ==========')
+trigram_lm = LaplaceLM(train_set, 3, 5)
+
+print("{}\n---Log-probability: {}\n".format(correct_sentence, trigram_lm.test(correct_sentence)))
+for test_sequence in test_sequences:
+    print("{}\n\n---Log-probability: {}\n".format(test_sequence, trigram_lm.test(test_sequence)))
+
+for predict_sequence in predict_sequences:
+    print(predict_sequence)
+    trigram_lm.predict(predict_sequence)
+
+trigram_lm.evaluate(test_set)
+
+print('\n========== Interpolated Language Model ==========')
+for l in np.linspace(0, 1, 6):
+    print('\nl1: {}, l2: {}'.format(l, 1-l))
+    interpolated_lm = InterpolatedLM(train_set, l, 5)
+    interpolated_lm.evaluate(test_set)
